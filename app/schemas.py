39b98c1e7338b1d79ac1ead0b0d459222bacf585
sebasdgc14 <sebasdgc14@gmail.com>
2025-11-13 12:11:05 -0500

    Updating scraping method and general schema for card structure

--- app/schemas.py
+++ app/schemas.py
@@ -6,37 +6,18 @@ class Card(BaseModel):
     A class to represent a card.
     """
 
-    id: int  # Unique identifier for the card OP01-001, ST02-002, etc.
+    unique_id: str
+    unique_img_link: str
+    print_set: str
+
+    id: str  # Unique identifier for the card OP01-001, ST02-002, etc.
     rarity: str  # C, UC, R, SR, SEC, SP, TR, L
     name: str  # Name of the card
-    type: str  # Straw Hat Crew / Marine / Warlord / Pirate / Revolutionary
+    card_type: list[str]  # Straw Hat Crew / Marine / Warlord / Pirate / Revolutionary
     color: list[str]  # Red, Green, Blue, Purple, Black, Yellow or combination A/B
-    effect: str | None  # Text description of the effect
-
-
-class LeaderCard(Card):
-    """
-    A class to represent a leader card.
-    """
-
-    attribute: list[str]  # Strike, Special, Slash, Wisdom, Ranged, ?
-    power: int  # Attack power
-
-
-class EventCard(Card):
-    """
-    A class to represent an event or stage card.
-    """
-
-    cost: int  # Don!! cost to play
-    trigger: str  # Effect when played from life
-
-
-class CharacterCard(LeaderCard):
-    """
-    A class to represent a character card.
-    """
-
-    cost: int  # Don!! cost to play
-    trigger: str  # Effect when played from life
-    counter: int  # Counter power
+    effect: str  # Text description of the effect
+    block: str  # Card block for rotation
+    attribute: list[str]
+    power: str
+    cost: str
+    counter: str
