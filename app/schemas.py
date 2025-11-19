c839b2b22db3734a0fcfb6e15d8c9428c47e8b0f
sebasdgc14 <sebasdgc14@gmail.com>
2025-11-19 12:44:40 -0500

    Updated schemas to store df

--- app/schemas.py
+++ app/schemas.py
@@ -13,11 +13,11 @@ class Card(BaseModel):
     id: str  # Unique identifier for the card OP01-001, ST02-002, etc.
     rarity: str  # C, UC, R, SR, SEC, SP, TR, L
     name: str  # Name of the card
-    card_type: list[str]  # Straw Hat Crew / Marine / Warlord / Pirate / Revolutionary
-    color: list[str]  # Red, Green, Blue, Purple, Black, Yellow or combination A/B
+    card_type: str  # Straw Hat Crew / Marine / Warlord / Pirate / Revolutionary
+    color: str  # Red, Green, Blue, Purple, Black, Yellow or combination A/B
     effect: str  # Text description of the effect
     block: str  # Card block for rotation
-    attribute: list[str]
+    attribute: str
     power: str
     cost: str
     counter: str
