b615da09143034bd4ff257ca1dd623f78231ae3e
sebasdgc14 <sebasdgc14@gmail.com>
2026-01-27 14:50:48 -0500

    Refactoring database info and tables

---
+++ db/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from core.config import DATABASE_URL, PATH_DATABASE_LOCAL
+
+SQLALCHEMY_DATABASE_URL = DATABASE_URL
+
+connect_args = {}
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(
+    f"sqlite:///{PATH_DATABASE_LOCAL}/cards.db", connect_args=connect_args
+)
+print("USING DATABASE:", engine.url)
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
